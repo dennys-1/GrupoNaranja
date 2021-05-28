3e2f231048f4687eb1ad56cb49dcbf448b474bc4
dennys <DENNYS_ORTIZ@USMP.PE>
2021-05-28 14:37:33 -0500

    Creacion vistas Controladores

---
+++ inicio/Controllers/AdministradorController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+//using NARANJA.Data;
+//using NARANJA.Models;
+
+namespace NARANJA.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        private readonly ILogger<AdministradorController> _logger;
+        
+       // private readonly ApplicationDbContext _context;
+
+        public AdministradorController(ILogger<AdministradorController> logger)/*,
+        /*ApplicationDbContext context)*/
+        {
+            _logger = logger;
+            //_context=context;
+        }
+         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Carta()
+        {
+            return View();
+        }
+         public IActionResult Reservas()
+        { 
+           return View();
+
+            /*var listreservas = _context.Reservas.ToList();
+            ViewData["message"]="";
+            return View(listreservas);*/
+        }
+         public IActionResult Horario()
+        {
+            return View();
+        }
+        public IActionResult Miperfil()
+        {
+            return View();
+        }
+    }
+}
--- inicio/Controllers/HomeController.cs
+++ inicio/Controllers/HomeController.cs
@@ -11,7 +11,8 @@ namespace inicio.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+       
+         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -21,6 +22,9 @@ namespace inicio.Controllers
         public IActionResult Index()
         {
             return View();
+        } public IActionResult Nosotros()
+        {
+            return View();
         }
 
         public IActionResult Privacy()
@@ -28,10 +32,11 @@ namespace inicio.Controllers
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+         {
+             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+         }
     }
 }
